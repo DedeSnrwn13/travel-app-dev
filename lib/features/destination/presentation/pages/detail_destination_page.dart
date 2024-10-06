9210fc6d7dc6cacd2e282221384a19c052071cc2
DedeSnrwn13 <sunarwandede13@gmail.com>
2024-10-06 14:40:08 +0700

    feat: detail page gallery

--- lib/features/destination/presentation/pages/detail_destination_page.dart
+++ lib/features/destination/presentation/pages/detail_destination_page.dart
@@ -1,5 +1,11 @@
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:staggered_grid_view_flutter/widgets/staggered_grid_view.dart';
+import 'package:staggered_grid_view_flutter/widgets/staggered_tile.dart';
+import 'package:travel_app/api/urls.dart';
 import 'package:travel_app/features/destination/domain/entities/destination_entity.dart';
+import 'package:travel_app/features/destination/presentation/widgets/circle_loading.dart';
 
 class DetailDestinationPage extends StatefulWidget {
   const DetailDestinationPage({super.key, required this.destination});
@@ -15,6 +21,95 @@ class _DetailDestinationPageState extends State<DetailDestinationPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        children: [
+          const SizedBox(height: 10),
+          gallery(),
+        ],
+      ),
+    );
+  }
+
+  Widget gallery() {
+    List patternGallery = [
+      const StaggeredTile.count(3, 3),
+      const StaggeredTile.count(2, 1.5),
+      const StaggeredTile.count(2, 1.5),
+    ];
+
+    return StaggeredGridView.countBuilder(
+      crossAxisSpacing: 16,
+      mainAxisSpacing: 16,
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      crossAxisCount: 5,
+      itemCount: 3,
+      staggeredTileBuilder: (index) {
+        return patternGallery[index % patternGallery.length];
+      },
+      itemBuilder: (context, index) {
+        if (index == 2) {
+          return ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                itemGalleryImage(index),
+                Container(
+                  color: Colors.black.withOpacity(0.3),
+                  alignment: Alignment.center,
+                  child: const Text(
+                    '+ More',
+                    style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                ),
+              ],
+            ),
+          );
+        }
+
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(12),
+          child: itemGalleryImage(index),
+        );
+      },
+    );
+  }
+
+  Widget itemGalleryImage(int index) {
+    return ExtendedImage.network(
+      URLs.image(widget.destination.images[index]),
+      fit: BoxFit.cover,
+      handleLoadingProgress: true,
+      loadStateChanged: (state) {
+        if (state.extendedImageLoadState == LoadState.failed) {
+          return AspectRatio(
+            aspectRatio: 16 / 9,
+            child: Material(
+              borderRadius: BorderRadius.circular(16),
+              color: Colors.grey[300],
+              child: const Icon(
+                Icons.broken_image,
+                color: Colors.black,
+              ),
+            ),
+          );
+        }
+
+        if (state.extendedImageLoadState == LoadState.loading) {
+          return AspectRatio(
+            aspectRatio: 16 / 9,
+            child: Material(
+              borderRadius: BorderRadius.circular(16),
+              color: Colors.grey[300],
+              child: const CircleLoading(),
+            ),
+          );
+        }
+
+        return null;
+      },
     );
   }
 
